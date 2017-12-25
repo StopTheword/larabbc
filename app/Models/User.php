67edc3fc03ff8f8970a7558f3a1548e772c7403f
cfl <1615092979@qq.com>
2017-12-25 08:22:42 +0000

    list topics created by user

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,4 +27,8 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
     //$tables = 'users';
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }

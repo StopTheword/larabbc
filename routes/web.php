06a087bf69a70ff168cd44d2cac135707b6e971a
cfl <1615092979@qq.com>
2017-12-26 01:09:15 +0000

    user can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,5 @@ Route::resource('users', 'UsersController', ['only'=> ['show','update','edit']])
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+//上传图片的url
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
